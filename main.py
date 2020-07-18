77a6c5ae9eef0063cbf77f5721613385fb3d16ad
whitehatjr <52084703+whitehatjr@users.noreply.github.com>
2020-07-18 20:38:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ data = df["temp"].tolist()
 def random_set_of_mean(counter):
     dataset = []
     for i in range(0, counter):
-        random_index= random.randint(0,len(data))
+        random_index= random.randint(0,len(data)-1)
         value = data[random_index]
         dataset.append(value)
     mean = statistics.mean(dataset)
